aeadda31b303c15c1fa565ab8f9f99bd82cbb468
l1ttps <l1ttps443@gmail.com>
2023-12-27 22:47:16 +0700

    feat: update state settings

--- src/app/pages/settings/Settings.tsx
+++ src/app/pages/settings/Settings.tsx
@@ -1,15 +1,41 @@
+import { ToggleSwitch } from 'flowbite-react';
+import { useMemo } from "react";
 import Wrapper from "../../comps/Wrapper";
-import { useAppDispatch } from "../../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { removePassword } from "../../redux/slices/passwordSlice";
+import { SettingsState, changeSettings } from '../../redux/slices/settingsSlice';
+
 const Settings = () => {
     const dispatch = useAppDispatch()
+    const settings = useAppSelector(state => state.settings)
+
+    const mappingStateToggle = useMemo(() => {
+        return [
+            { key: "isEnableLock", label: "Enable Lock" },
+            { key: "isShowPasswordHint", label: "Show Password Hint" }
+        ].map(({ key, label }) => ({
+            key,
+            state: settings[key],
+            label,
+        }));
+    }, [settings]);
+
+    const handleChangeState = (checked: boolean, key: keyof SettingsState) => {
+        console.log(checked, key);
+        dispatch(changeSettings({
+            checked,
+            key
+        }))
+    }
     return (
         <Wrapper>
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Settings
             </h1>
-
-            {/* <ToggleSwitch checked={true} label="Toggle me" onChange={() => { }} /> */}
+            {
+                mappingStateToggle.map((toggle) =>
+                    <ToggleSwitch key={toggle.key} name={toggle.label} checked={toggle.state} label={toggle.label} onChange={(checked) => handleChangeState(checked, toggle.key as any)} />)
+            }
             <button onClick={() => dispatch(removePassword())}>remove password</button>
         </Wrapper>
     );
--- src/app/redux/slices/settingsSlice.ts
+++ src/app/redux/slices/settingsSlice.ts
@@ -1,9 +1,15 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
-interface SettingsState {
+export interface SettingsState {
     isShowPasswordHint: boolean,
     isEnableLock: boolean
 }
+
+interface ChangeSettingsPayload {
+    checked: boolean,
+    key: keyof SettingsState
+}
+
 const initialState: SettingsState = {
     isShowPasswordHint: true,
     isEnableLock: true
@@ -13,10 +19,21 @@ export const settingsSlice = createSlice({
     name: 'settings',
     initialState,
     reducers: {
-
+        /**
+         * Updates the settings state based on the provided action payload.
+         *
+         * @param {object} state - The current state of the settings.
+         * @param {object} action - The action object containing the payload.
+         * @param {string} action.payload.key - The key of the setting to be updated.
+         * @param {boolean} action.payload.checked - The new value for the setting.
+         */
+        changeSettings: (state, action: PayloadAction<ChangeSettingsPayload>) => {
+            const { key, checked } = action.payload;
+            state[key] = checked
+        }
     },
 });
 
-export const { } = settingsSlice.actions;
+export const { changeSettings } = settingsSlice.actions;
 
 export default settingsSlice.reducer;
