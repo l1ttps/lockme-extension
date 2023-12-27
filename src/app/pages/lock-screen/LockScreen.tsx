93119e45ea011fa980b77607f9f5c55c4fd0b7e5
l1ttps <l1ttps443@gmail.com>
2023-12-27 22:52:38 +0700

    feat: show password hint in lock screen

--- src/app/pages/lock-screen/LockScreen.tsx
+++ src/app/pages/lock-screen/LockScreen.tsx
@@ -10,7 +10,8 @@ import { LockScreenForm } from "../../type/types";
 
 const LockScreen = () => {
     const { register, handleSubmit, setError, formState: { errors } } = useForm();
-    const { hash } = useAppSelector(state => state.password)
+    const { hash, hint } = useAppSelector(state => state.password)
+    const { isShowPasswordHint } = useAppSelector(state => state.settings)
     const [update, setUpdate] = useState(new Date().getTime())
     const { disabledExpires } = useAppSelector(state => state.lock)
     const dispatch = useDispatch()
@@ -48,8 +49,6 @@ const LockScreen = () => {
             event.preventDefault();
         }
     };
-
-
     return (
         <div tabIndex={0} onKeyDown={handleKeyDown}>
             <Wrapper>
@@ -59,6 +58,7 @@ const LockScreen = () => {
                         <input autoFocus {...register("password", { required: true })} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                     </div>
                     {errors.password && <span className='text-red-500'>{errors.password.message?.toString()}</span>}
+                    {isShowPasswordHint && <div className='text-center text-gray-400'>Password Hint: {hint}</div>}
                     <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Unlock</button>
 
                 </form>
