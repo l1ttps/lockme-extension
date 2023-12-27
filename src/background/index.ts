e56dbedb685914f7154b43e8500f0de7aa262bc0
l1ttps <l1ttps443@gmail.com>
2023-12-27 21:45:04 +0700

    fix: block all action when press fn button

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,21 +1,19 @@
-import browser from 'webextension-polyfill';
 
 import store from '../app/redux/store';
-import { isDev } from '../shared/utils';
 
 store.subscribe(() => {
   console.log('state', store.getState());
 });
 
 
-// show welcome page on new install
-browser.runtime.onInstalled.addListener(async (details) => {
-  if (details.reason === 'install') {
-    //show the welcome page
-    const url = browser.runtime.getURL(isDev ? 'src/options/options.html#/welcome' : 'options.html#/welcome'); // TODO: better approach
-    await browser.tabs.create({ url });
-  }
-});
+// // show welcome page on new install
+// browser.runtime.onInstalled.addListener(async (details) => {
+//   if (details.reason === 'install') {
+//     //show the welcome page
+//     const url = browser.runtime.getURL(isDev ? 'src/options/options.html#/welcome' : 'options.html#/welcome'); // TODO: better approach
+//     await browser.tabs.create({ url });
+//   }
+// });
 
 // browser.runtime.onStartup.addListener(async () => {
 //   // Show the welcome page
@@ -25,16 +23,6 @@ browser.runtime.onInstalled.addListener(async (details) => {
 //     type: "popup",
 //     state: 'fullscreen'
 //   })
-
-//   if (windowId) {
-//     chrome.windows.onRemoved.addListener((windowId) => {
-//       if (windowId === windowId) {
-//         browser.runtime.getPlatformInfo().then(res => {
-
-//         });
-//       }
-//     });
-//   }
 // })
 export { };
 
