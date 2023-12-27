cb253795dab4b40f4ff8790100972266dd84000c
l1ttps <l1ttps443@gmail.com>
2023-12-27 21:45:04 +0700

    fix: block all action when press fn button

--- src/app/pages/lock-screen/LockScreen.tsx
+++ src/app/pages/lock-screen/LockScreen.tsx
@@ -1,5 +1,5 @@
 import * as bcrypt from "bcryptjs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
@@ -31,6 +31,16 @@ const LockScreen = () => {
         }
     };
 
+    useEffect(() => {
+        // Add event listener to the whole document
+        document.addEventListener('keydown', handleKeyDown);
+
+        // Cleanup the event listener on component unmount
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
 
     const handleKeyDown = (event: any) => {
         console.log(event.key);
@@ -41,12 +51,12 @@ const LockScreen = () => {
 
 
     return (
-        <div onKeyDown={handleKeyDown}>
+        <div tabIndex={0} onKeyDown={handleKeyDown}>
             <Wrapper>
                 <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)}>
                     <div>
                         <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                        <input {...register("password", { required: true })} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                        <input autoFocus {...register("password", { required: true })} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                     </div>
                     {errors.password && <span className='text-red-500'>{errors.password.message?.toString()}</span>}
                     <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Unlock</button>
