0722ffa6751bbffaaec80c82a9fa1329a26cc474
l1ttps <l1ttps443@gmail.com>
2023-12-30 14:00:05 +0700

    feat: add loading authenticating

--- src/app/pages/lock-screen/UnlockWithPasskeys.tsx
+++ src/app/pages/lock-screen/UnlockWithPasskeys.tsx
@@ -1,18 +1,35 @@
-import { Button } from "flowbite-react";
-import { useCallback } from "react";
+import { Button, Spinner } from "flowbite-react";
+import { useCallback, useState } from "react";
 import unlockWithPasskeys from "../../../utils/unlockWithPasskeys";
 import { useAppSelector } from "../../redux/hooks";
 
 const UnlockWithPasskeys = () => {
     const objPasskeys = useAppSelector(state => state.passkeys)
+    const [loading, setLoading] = useState(false)
+
     const passkeys = Object.keys(objPasskeys)
     const handleUnLockWithPasskeys = useCallback(async () => {
-        unlockWithPasskeys(passkeys[0])
+        setLoading(true)
+        unlockWithPasskeys(passkeys[0]).finally(() => {
+            done()
+        })
     }, [])
+
+    const done = useCallback(() => {
+        setLoading(false)
+    }, [])
+
     if (passkeys.length === 0) return <></>
     return (
         <div>
-            <Button onClick={handleUnLockWithPasskeys} fullSized color="gray">Use passkeys</Button>
+            <Button disabled={loading} onClick={handleUnLockWithPasskeys} fullSized color="gray">
+                {loading ? <div className="flex items-center gap-x-3">
+                    <Spinner size={"sm"} color="info" aria-label="Info spinner example" />
+                    <span>Authenticating</span>
+                </div> :
+                    <span>Use passkeys</span>
+                }
+            </Button>
         </div>
     );
 };
